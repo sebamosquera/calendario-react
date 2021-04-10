dadb37b65ea15d7d49ebc87ed6e1b235e69ce73e
Sebastian <sebastianmosquera86@gmail.com>
2021-04-09 23:47:54 -0300

    better transitions, changes made in day.css and month.css

--- src/Day/Day.js
+++ src/Day/Day.js
@@ -11,7 +11,13 @@ const Day = (props) => {
       {props.miniatura === true ?
         <div>
           <h1 className='day-number'>{props.day}</h1>
-          <div>TAREAS</div>
+          <div className='day-agregar-tarea'>Agregar Tarea</div>
+          {
+          <ul className='day-list'>
+            <li>Tarea 1</li>
+            <li>Tarea 2</li>
+          </ul>
+        }
         </div> :
         <div>FECHA: {selectedDate}</div>
       }
